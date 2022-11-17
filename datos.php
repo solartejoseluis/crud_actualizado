07d909be8fa725a173f1f6e871de8bd3529c350a
solartejoseluis <solartejoseluis@gmail.com>
2022-11-17 17:17:28 -0500

    adecuar funcion agregar datos
    se actualizó el codigo del modal a la version 5.2 y se conectó el boton.
    no agraga los datos del formulario a la base de datos
    al parecer el problema viene al recoger los datos del formulario html por el js

--- datos.php
+++ datos.php
@@ -1,7 +1,6 @@
 <?php
 header('Content-Type: application/json');
 require "pdo.php";
-//$conexion = $conn();
 
 switch ($_GET['accion']) {
     case 'listar':
@@ -12,40 +11,36 @@ switch ($_GET['accion']) {
         echo json_encode($result);
         break;
 
-        
-    // la siguiente seccion no envia los nombres de campos y por eso falla.    
-    // case 'listar':
-    //     $sql = "SELECT codigo,descripcion,precio FROM articulos";
-    //     $stmt = $pdo -> prepare($sql);
-    //     $stmt -> execute();
-        
-    //     $result = $stmt -> fetchAll(PDO::FETCH_FUNC, fn($codigo, $descripcion, $precio) => [$codigo, $descripcion, $precio]);
-        
-    //     echo json_encode(["data" => $result]);
-    //     break;
-
-/*     case 'agregar':
-        $sql = ($conexion, "INSERT INTO articulos(descripcion,precio) VALUES ('$_POST[descripcion]',$_POST[precio])");
+     case 'agregar':
+        $sql = "INSERT INTO articulos(descripcion,precio) VALUES ('$_POST[descripcion]',$_POST[precio])";
         echo json_encode($sql);
         break;
 
-    case 'borrar':
-        $respuesta = mysqli_query($conexion, "delete from articulos where codigo=$_GET[codigo]");
-        echo json_encode($respuesta);
-        break;
+        try {
+            $sql = "INSERT INTO articulos(descripcion,precio) VALUES ('$_POST[descripcion]',$_POST[precio])";
+            $response = $conn->exec($sql);
+            echo json_encode($response);
+            } catch(PDOException $e) {
+                echo $sql . "<br>" . $e->getMessage();
+                }
 
-    case 'consultar':
-        $datos = mysqli_query($conexion, "select codigo,descripcion,precio from articulos where codigo=$_GET[codigo]");
-        $resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
-        echo json_encode($resultado);
-        break;
+    // case 'borrar':
+    //     $respuesta = mysqli_query($conexion, "DELETE FROM articulos where codigo=$_GET[codigo]");
+    //     echo json_encode($respuesta);
+    //     break;
 
-    case 'modificar':
-        $respuesta = mysqli_query($conexion, "update articulos set
-        descripcion='$_POST[descripcion]',
-        precio=$_POST[precio]
-        where codigo=$_GET[codigo]");
-        echo json_encode($respuesta);
-        break; */
+    // case 'consultar':
+    //     $datos = mysqli_query($conexion, "select codigo,descripcion,precio from articulos where codigo=$_GET[codigo]");
+    //     $resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
+    //     echo json_encode($resultado);
+    //     break;
+
+    // case 'modificar':
+    //     $respuesta = mysqli_query($conexion, "update articulos set
+    //     descripcion='$_POST[descripcion]',
+    //     precio=$_POST[precio]
+    //     where codigo=$_GET[codigo]");
+    //     echo json_encode($respuesta);
+    //     break;
 }
 ?>
