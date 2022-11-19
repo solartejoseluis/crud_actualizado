51523f26a2cda85aaf696ac4069c067ae0ca62b2
solartejoseluis <solartejoseluis@gmail.com>
2022-11-19 11:17:51 -0500

    ajustado funcionamiento de todo el crud
    - Se ajusta y verifica que los procesos listar, agregar, modificar y borrar funcionan adecuadamente.
    - Se considera esta adecuación como el crud actualizado base ya terminado.
    - A partir de este es posible iniciar otros proyectos.

--- datos.php
+++ datos.php
@@ -3,39 +3,38 @@ header('Content-Type: application/json');
 require "pdo.php";
 
 switch ($_GET['accion']) {
-    case 'listar':
-        $sql = "SELECT codigo,descripcion,precio FROM articulos";
-        $stmt = $pdo -> prepare($sql);
-        $stmt -> execute();
-        $result = $stmt -> fetchAll(PDO::FETCH_ASSOC);
-        echo json_encode($result);
-        break;
+case 'listar':
+    $sql = "SELECT codigo,descripcion,precio FROM articulos";
+    $stmt = $pdo -> prepare($sql);
+    $stmt -> execute();
+    $result = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($result);
+    break;
 
-     case 'agregar':
-            $sql = "INSERT INTO articulos(descripcion,precio) VALUES ('$_POST[descripcion]',$_POST[precio])";
-            $response = $pdo->exec($sql);
-            echo json_encode($response);
-            break;
+case 'agregar':
+    $sql = "INSERT INTO articulos(descripcion,precio) VALUES ('$_POST[descripcion]',$_POST[precio])";
+    $response = $pdo->exec($sql);
+    echo json_encode($response);
+    break;
 
-    case 'borrar':
-         $sql = "DELETE FROM articulos where codigo=$_GET[codigo]";
-         echo json_encode($sql);
-         break;
+case 'borrar':
+    $sql = "DELETE FROM articulos where codigo=$_GET[codigo]";
+    $response = $pdo->exec($sql);
+    echo json_encode($response);
+    break;
 
-    case 'consultar':
-        $sql = "SELECT codigo,descripcion,precio FROM articulos WHERE codigo=$_GET[codigo]";
-        $stmt = $pdo -> prepare($sql);
-        $stmt -> execute();
-        $result = $stmt -> fetchAll(PDO::FETCH_ASSOC);
-        echo json_encode($result);
-        break;
+case 'consultar':
+    $sql = "SELECT codigo,descripcion,precio FROM articulos WHERE codigo=$_GET[codigo]";
+    $stmt = $pdo -> prepare($sql);
+    $stmt -> execute();
+    $result = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($result);
+    break;
 
-    case 'modificar':
-         $sql = "UPDATE articulos SET
-         descripcion='$_POST[descripcion]',
-         precio=$_POST[precio]
-         WHERE codigo=$_GET[codigo]";
-         echo json_encode($sql);
-         break;
+case 'modificar':
+    $sql = "UPDATE articulos SET descripcion='$_POST[descripcion]',precio=$_POST[precio] WHERE codigo=$_GET[codigo]";
+    $response = $pdo->exec($sql);
+    echo json_encode($response);
+    break;
 }
 ?>
